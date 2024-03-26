058db1dac801f9976c736cfe4b473fb16f9388aa
Francisco Conceição <franciscodnconceicao@gmail.com>
2024-03-26 18:13:10 +0000

    [FIX] - Alterations on the module of NewsDate

--- Backend/app/api/services/News/NewsDate.py
+++ Backend/app/api/services/News/NewsDate.py
@@ -1,8 +1,10 @@
 import requests
 from fastapi import APIRouter
 
+from app.api.models.News.DateNews import DateNews
+
 NewsDateRouter = APIRouter()
 @NewsDateRouter.post("/news/date/get/")
 async def getCompanyImage(params:DateNews):
     date = params.date
-    limit =
+    limit = params.limit_news

9f30249710cf3a11db430917ed2d049273500d56
Francisco Conceição <Francisco Conceio@CONCEIO-PC>
2024-03-31 21:07:41 +0100

    [IMP] - Implementation of SQL Alchemy to the backend

--- Backend/app/const.py
+++ Backend/app/const.py
@@ -2,5 +2,6 @@ headers = {
     "accept": "application/json",
     "APCA-API-KEY-ID": "PKWD0U5VC3Z0XB7QA09E",
     "APCA-API-SECRET-KEY": "SDs8F7vW2b1cyKWSqDLj3TJWwa5111FzbVH8RuAg",
-    "API-NINJA-KEY": "W991gcZMI4U+lLRG/Qm/LQ==95RG9bdegtlGlhDe"
+    "API-NINJA-KEY": "W991gcZMI4U+lLRG/Qm/LQ==95RG9bdegtlGlhDe",
+    "SQL_ALCHEMY_DATABASE_URL": "postgresql://localhost:5431/postgres"
 }
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,6 +3,10 @@ import uvicorn
 import app.api.services.Stocks.datetimeStocksServices as serviceStockDate
 from fastapi.middleware.cors import CORSMiddleware
 import app.api.services.Stocks.companyLogo as companyLogoService
+from sqlalchemy import create_engine
+from const import headers
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 app.include_router(serviceStockDate.RouterDatetimeStocksServices)
@@ -18,5 +22,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+engine = create_engine(
+    headers['SQL_ALCHEMY_DATABASE_URL'], connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 if __name__ == "__main__":
+    Base = declarative_base()
     uvicorn.run(app)
